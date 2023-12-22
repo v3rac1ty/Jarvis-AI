72c94ede30e572748730067017a2dca9f6551a35
Rishi Vemulapalli <rishivemulapalli@gmail.com>
2023-12-22 00:26:07 -0500

    Update speech.py

--- speech.py
+++ speech.py
@@ -5,7 +5,7 @@ import openai
 import speech_recognition as sr
 
 
-API_KEY = "insert your API key here"
+API_KEY = "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"
 
 lang='en'
 exitProg = False;
@@ -17,71 +17,28 @@ while True:
         r = sr.Recognizer()
         with sr.Microphone() as source:
             audio = r.listen(source)
-            said = ""
+            input = ""
 
             try:
-                said = r.recognize_google(audio)
-                print(said)
+                input = r.recognize_google(audio)
+                print(input)
 
-                if "Jarvis" in said:
+                if "Jarvis" in input:
                     completion = openai.ChatCompletion.create(
                         model = "gpt-3.5-turbo", 
-                        messages = [{"role": "user", "content": said}])
+                        messages = [{"role": "user", "content": input}])
                     text = completion['choices'][0].message.content
                     speech = gTTS(text=text, lang=lang, slow=False, tld="co.uk")
                     speech.save("output.mp3")
                     playsound.playsound("output.mp3")
+                    if "shut down" in input:
+                        exitProg = True
             except Exception:
                 print("Error")
-            if "Jarvis shut down" in said:
-                exitProg = True
 
-        return said
+        return input
     
     if exitProg:
         break
 
-    get_audio(import os
-import time
-import pyaudio
-import playsound
-from gtts import gTTS
-import openai
-import speech_recognition as sr
-
-
-API_KEY = "your-api-key"
-
-lang='en'
-exitProg = False;
-
-openai.api_key = API_KEY
-
-while True:
-    def get_audio():
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            audio = r.listen(source)
-            said = ""
-
-            try:
-                said = r.recognize_google(audio)
-                print(said)
-
-                if "Jarvis" in said:
-                    completion = openai.ChatCompletion.create(
-                        model = "gpt-3.5-turbo", 
-                        messages = [{"role": "user", "content": said}])
-                    text = completion['choices'][0].message.content
-                    speech = gTTS(text=text, lang=lang, slow=False, tld="")
-                    speech.save("output.mp3")
-                    playsound.playsound("output.mp3")
-            except Exception:
-                print("Error")
-            if "Jarvis shut down" in said:
-                exitProg = True
-
-        return said
-    if exitProg:
-        break
     get_audio()
