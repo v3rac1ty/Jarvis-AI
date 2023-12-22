e00e5a2388d8b048b12c6fa53ca6d055b4bf95d4
Rishi Vemulapalli <rishivemulapalli@gmail.com>
2023-12-22 00:07:37 -0500

    Jarvis Test 1

--- speech.py
+++ speech.py
@@ -1,51 +1,44 @@
-import speech_recognition as sr
-import pyttsx3
+import os
+import time
+import pyaudio
+import playsound
+from gtts import gTTS
 import openai
+import speech_recognition as sr
 
-# Initialize the speech recognition engine
-r = sr.Recognizer()
 
-# Initialize the text-to-speech engine
-engine = pyttsx3.init()
+API_KEY = "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"
 
-# Set the OpenAI API key
-openai.api_key = 'YOUR_API_KEY'
+lang='en'
+exitProg = False;
 
-# Function to convert speech to text
-def speech_to_text():
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            return text
-        except sr.UnknownValueError:
-            print("Sorry, I could not understand your speech.")
-        except sr.RequestError as e:
-            print("Sorry, an error occurred while processing your request. Please try again later.")
-        return ""
+openai.api_key = API_KEY
 
-# Function to generate voice response using GPT model
-def generate_voice_response(input_text):
-    response = openai.Completion.create(
-        engine='davinci',
-        prompt=input_text,
-        max_tokens=100
-    )
-    return response.choices[0].text.strip()
-
-# Function to convert text to speech
-def text_to_speech(text):
-    engine.say(text)
-    engine.runAndWait()
-
-# Main program loop
 while True:
-    # Take voice input
-    input_text = speech_to_text()
+    def get_audio():
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            said = ""
 
-    # Generate voice response
-    response_text = generate_voice_response(input_text)
+            try:
+                said = r.recognize_google(audio)
+                print(said)
 
-    # Convert response text to speech
-    text_to_speech(response_text)
+                if "Jarvis" in said:
+                    completion = openai.ChatCompletion.create(
+                        model = "gpt-3.5-turbo", 
+                        messages = [{"role": "user", "content": said}])
+                    text = completion['choices'][0].message.content
+                    speech = gTTS(text=text, lang=lang, slow=False, tld="")
+                    speech.save("output.mp3")
+                    playsound.playsound("output.mp3")
+            except Exception:
+                print("Error")
+            if "Jarvis shut down" in said:
+                exitProg = True
+
+        return said
+    if exitProg==False:
+        break
+    get_audio()
